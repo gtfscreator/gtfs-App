3b0cd4d10c41bba0b5e39a40e2c5daa5d2a13bfb
mrjungle <davide.nuccio@gmail.com>
2015-07-14 16:33:11 +0200

    test - aggiunta service

---
+++ www/js/service.js
@@ -0,0 +1,24 @@
+angular.module('gtfsApp.services', [])
+
+
+.factory('Type', function($http, $q) {
+
+  var dizType = {
+    1 : 'ciao',
+    2 : 'Metropolitana',
+    3 : 'bus',
+    4 : 'nave',
+    5 : 'mboooo'
+  };
+
+  var tipo = function(id){
+  return dizType[id];
+  console.log(dizType[id]);
+  //console.log('ciao');
+  }
+
+  return { 'humanize' : tipo };
+
+
+})
+
